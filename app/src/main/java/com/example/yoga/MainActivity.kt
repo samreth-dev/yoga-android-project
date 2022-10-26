c9c21bbea870734f83554a682188e167601452ae
SokcheaMIU <sokchea.chim@miu.edu>
2022-10-26 14:26:49 -0500

    Resolved conflict - Sam

--- app/src/main/java/com/example/yoga/MainActivity.kt
+++ app/src/main/java/com/example/yoga/MainActivity.kt
@@ -16,6 +16,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
@@ -30,6 +31,11 @@ class MainActivity : AppCompatActivity() {
 
     private var isEdited = false
     lateinit var mnavController: NavController
+    private var articleFragment = ArticleFragment()
+    private var calorieLostFragment = CalorieLostFragment()
+    private var yogaIntroFragment = YogaIntroFragment()
+    private var profileFragment = ProfileFragment()
+    private var articleDetailFragment = ArticleDetailFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,6 +74,5 @@ class MainActivity : AppCompatActivity() {
             true
         }
     }
-
 
 }
